0ea65695e3c08e9e31f755e6c2f4e623e81da532
SaladRenton <tpalenfanta@gmail.com>
2024-10-23 14:22:40 -0300

    "Updated package-lock.json, package.json, public/index.html, src/App.js, and src/index.css files"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+/*import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import NotFound from './NotFound';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default App;*/
+
+
+
+
+  import './components/categories/categories.styles.scss'
+import Directory from './components/directory-item/directory.component.jsx';
+import Categories from './components/categories/categories.component.jsx';
+
+const App = () => {
+  
+
+  return (
+    <div className='categories-container'>
+      <Directory categories={Categories()} />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
