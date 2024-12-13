4122d3624d1bb6077c1473b8a785a39ad1025198
tpalenfanta@gmail.com <tpalenfanta@gmail.com>
2024-12-13 17:34:00 -0300

    stripe impemented

---
+++ netlify/functions/create-payment-intent.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+  try {
+    const { amount } = JSON.parse(event.body);
+    const paymentIntent = await stripe.paymentIntents.create({
+      // Cambiado a paymentIntents
+      amount,
+      currency: "usd",
+      payment_method_types: ["card"],
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ paymentIntent }),
+    };
+  } catch (error) {
+    console.log({ error });
+    return {
+      statusCode: 400, // Cambiado a statusCode
+      body: JSON.stringify({ error: error.message }), // Solo el mensaje de error
+    };
+  }
+};
--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,37 @@
-// import { compose, createStore, applyMiddleware } from 'redux';
 import { configureStore } from "@reduxjs/toolkit";
-// import { persistStore, persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
 import logger from "redux-logger";
-
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { rootReducer } from "./root-reducer";
 
+// Configuración del middleware
 const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
   Boolean
 );
 
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(middleWares),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST"], // Ignorar la acción de persistencia
+        ignoredPaths: ["register"], // Ignorar la ruta de registro
+      },
+    }).concat(middleWares), // Agregar middlewares adicionales
 });
 
+export const persistor = persistStore(store);
+
 /* 
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+
 
 
 
@@ -31,11 +44,7 @@ const composeEnhancer =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['user'],
-};
+
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
@@ -47,5 +56,5 @@ const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
   composedEnhancers
 );
 
-export const persistor = persistStore(store);
+
  */
